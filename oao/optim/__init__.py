bf0082d98b2bf5cc0e75d6c7f75252480fe9c25c
NeptuneProjects <william.f.jenkins@gmail.com>
2023-04-29 14:55:46 -0700

    Rm depr version

--- oao/optim/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2023 by William Jenkins
-# Scripps Institution of Oceanography
-# University of California San Diego
-#
-# This source code is licensed under the MIT license found in the LICENSE
-# file in the root directory of this source tree.
-
-"""Module contains submodules for running optimization experiments."""
--- tests/test_e2e.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Testing end-to-end optimization workflow..."
-python3 ./scripts/config.py && \
-python3 ./scripts/demo.py && \
-echo "Optimization workflow completed."
