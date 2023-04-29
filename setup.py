9db8c745dbb41c109f1d06f0e4d5989f6f1d99ff
NeptuneProjects <william.f.jenkins@gmail.com>
2023-04-29 16:05:42 -0700

    Change setup to python from toml

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+import oao
+
+setup(
+    name="OAOptimization",
+    version=oao.__version__,
+    description="Optimization library for ocean acoustics.",
+    package_dir={"": "oao"},
+    packages=find_packages(where="oao"),
+    url="https://github.com/NeptuneProjects/OAOptimization",
+    author="William Jenkins",
+    author_email="wjenkins@ucsd.edu",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent"
+    ],
+    install_requires=[
+        "ax-platform"
+    ],
+    python_requires=">=3.10",
+)
