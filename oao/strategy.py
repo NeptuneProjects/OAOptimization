8e2431df8af139fa0f197ad625bae3e116e48817
NeptuneProjects <william.f.jenkins@gmail.com>
2023-08-29 23:36:11 -0700

    Simplify grid strategy

--- oao/strategy.py
+++ oao/strategy.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
 from typing import Optional, Union
 
 
@@ -9,11 +8,6 @@ class GridStrategy:
         self,
         num_trials: Union[int, list[int]],
         max_parallelism: Optional[int] = None,
-        *args,
-        **kwargs,
     ) -> None:
         self.num_trials = num_trials
         self.max_parallelism = max_parallelism
-
-    def __call__(self, *args, **kwargs):
-        return lambda: self.num_trials
